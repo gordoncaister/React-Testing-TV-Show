8b227c59bef78dbff85800a13d715d12ff87f5c5
Gordon Caister <gordoncaister@gmail.com>
2020-04-20 19:52:52 -0500

    test working for episodes.test.js

--- src/components/Episodes.te.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render, fireEvent, wait } from "@testing-library/react";
-import Episodes from "./Episodes";
-
-test ("episodes renders", () => {
-    render (<Episodes />)
-})
